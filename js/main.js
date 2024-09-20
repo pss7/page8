70a8ddd118aaf8018fa8e230961d33786d21fa69
parksangseok <>
2024-09-20 17:04:34 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ $(function () {
 
 });
 
+$(window).on('load resize', function () {
+
+    var resizeWidth = $(this).width();
+
+    if (resizeWidth < 1025) {
+
+        /* section02-slick */
+        $('#section02 .slickWrap .slick').slick({
+            autoplay: false,
+            infinite: true,
+            slidesToShow: 1,
+            dots: false,
+            arrows: true,
+            prevArrow: $('#section02 .control .prev'),
+            nextArrow: $('#section02 .control .next'),
+        });
+
+    }
+
+})

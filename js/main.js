d8d313fb573b16c063a056bcd3f05d6bc83d3de8
parksangseok <>
2024-09-20 15:03:02 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function () {
 
+    /* section14-slick */
+    $('#section01 .slick').slick({
+        autoplay: false,
+        infinite: true,
+        slidesToShow: 1,
+        dots: true,
+        arrows: false,
+    });
 
 });
 

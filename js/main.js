84f1d3bfdcccaf729bee13d49e48d322fb00d747
parksangseok <>
2024-09-22 23:32:44 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -11,23 +11,26 @@ $(function () {
 
 });
 
-$(window).on('load resize', function () {
 
+$(window).on('load resize', function () {
     var resizeWidth = $(this).width();
 
     if (resizeWidth < 1025) {
-
-        /* section02-slick */
-        $('#section02 .slickWrap .slick').slick({
-            autoplay: false,
-            infinite: true,
-            slidesToShow: 1,
-            dots: false,
-            arrows: true,
-            prevArrow: $('#section02 .control .prev'),
-            nextArrow: $('#section02 .control .next'),
-        });
-
+        if (!$('#section02 .slickWrap .slick').hasClass('slick-initialized')) {
+            $('#section02 .slickWrap .slick').slick({
+                autoplay: false,
+                infinite: true,
+                slidesToShow: 1,
+                dots: false,
+                arrows: true,
+                prevArrow: $('#section02 .control .prev'),
+                nextArrow: $('#section02 .control .next'),
+            });
+        }
+    } else {
+        // 넓은 화면에서 슬라이더 초기화 해제
+        if ($('#section02 .slickWrap .slick').hasClass('slick-initialized')) {
+            $('#section02 .slickWrap .slick').slick('unslick');
+        }
     }
-
-})
+});
